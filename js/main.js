b712ad06abc262a92eed67270fe4e3877c6b09f8
Hugo.Lin <hugo@certitraining.org>
2016-02-20 17:03:33 +0800

    finish

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    	anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    	menu: '#menu',
+    	scrollingSpeed: 1000
+    });
+
+    $("#demosMenu").change(function(){
+    	window.location.href = $(this).find("option:selected").attr("id") + '.html';
+    });
+
+    $('.ui.small.circular.image')
+      .popup({
+      })
+    ;
+
+});
